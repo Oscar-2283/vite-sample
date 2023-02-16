0ba1e5267c714a1ad7eadc7a13626326fbe6a7a6
Oscar <93570814+Oscar-2283@users.noreply.github.com>
2023-02-16 15:33:20 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "vite-sample/",
+  base: "vue-training/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
